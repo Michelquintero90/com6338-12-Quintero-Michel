fa9a9ac2b1b17776e859daa9708b9f7e113d7e2b
Michel <michel.quintero24@gmail.com>
2024-08-04 18:21:52 -0400

    Assigment12

---
+++ index.js
@@ -0,0 +1,97 @@
+const words = [
+  'bananas',
+  'grapes',
+  'carousel',
+  'milkshake',
+  'javascript',
+  'limousine',
+  'chocolate',
+  'programming',
+  'meatloaf',
+  'ukulele',
+  'mango'
+];
+
+let wins = 0;
+let losses = 0;
+let currentWord;
+
+class Word {
+  constructor(word) {
+    this.word = word;
+    this.displayWord = word.replace(/[\w]/g, "_");
+    this.remainingGuesses = 10;
+    this.incorrectLetters = [];
+    this.correctLetters = [];
+  }
+
+  guessLetter(letter) {
+    if (this.word.includes(letter)) {
+      if (!this.correctLetters.includes(letter)) {
+        this.correctLetters.push(letter);
+      }
+      this.displayWord = this.word
+        .split('')
+        .map(char => this.correctLetters.includes(char) ? char : '_')
+        .join('');
+    } else {
+      if (!this.incorrectLetters.includes(letter)) {
+        this.incorrectLetters.push(letter);
+        this.remainingGuesses--;
+      }
+    }
+  }
+
+  updateScreen() {
+    document.getElementById('word-to-guess').textContent = this.displayWord;
+    document.getElementById('incorrect-letters').textContent = this.incorrectLetters.join(', ');
+    document.getElementById('remaining-guesses').textContent = this.remainingGuesses;
+  }
+
+  isGameOver() {
+    return this.remainingGuesses <= 0 || this.word === this.displayWord;
+  }
+
+  getWinOrLoss() {
+    if (this.word === this.displayWord && this.remainingGuesses > 0) {
+      return 'win';
+    } else if (this.remainingGuesses <= 0) {
+      return 'loss';
+    }
+    return null;
+  }
+}
+
+function newGame() {
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  currentWord = new Word(randomWord);
+  currentWord.updateScreen();
+}
+
+document.onkeyup = function(e) {
+  const pressedKey = e.key.toLowerCase();
+  if (!/^[a-z]{1}$/g.test(pressedKey)) return;
+
+  currentWord.guessLetter(pressedKey);
+  currentWord.updateScreen();
+
+  const gameOver = currentWord.isGameOver();
+
+  if (gameOver) {
+    const previousWord = document.getElementById('previous-word');
+    const winDisplay = document.getElementById('wins');
+    const lossDisplay = document.getElementById('losses');
+    previousWord.textContent = currentWord.word;
+    const result = currentWord.getWinOrLoss();
+    if (result === 'win') {
+      wins++;
+      winDisplay.textContent = wins;
+    } else if (result === 'loss') {
+      losses++;
+      lossDisplay.textContent = losses;
+    }
+    newGame();
+  }
+}
+
+newGame();
